4526c285fa8a6f2a88891e11ac0c6cf05ba870db
juarisco <juarisco@outlook.com>
2018-12-18 10:59:31 -0500

    Displaying products

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class FrontEndController extends Controller
+{
+    public function index()
+    {
+        return view('index', ['products' => Product::paginate(3)]);
+
+    }
+}
