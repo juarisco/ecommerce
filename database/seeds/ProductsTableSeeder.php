87a42648897f7b1902cf06cb2a3038af6d74a4aa
juarisco <juarisco@outlook.com>
2018-12-18 10:05:49 -0500

    Products factory

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -12,22 +12,24 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        $p1 = [
-            'name' => 'Learn to build websites in HTML5',
-            'image' => 'uploads/products/book.png',
-            'price' => 5000,
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum et magna vitae iaculis. Aenean efficitur, nisi at rhoncus pulvinar, augue nunc pretium magna, eget mattis massa turpis id lacus. Pellentesque non eros quis nisi lobortis pulvinar. Donec et dui at mi eleifend tincidunt. Curabitur ultricies quam non justo varius.'
-        ];
+        // $p1 = [
+        //     'name' => 'Learn to build websites in HTML5',
+        //     'image' => 'uploads/products/book.png',
+        //     'price' => 5000,
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum et magna vitae iaculis. Aenean efficitur, nisi at rhoncus pulvinar, augue nunc pretium magna, eget mattis massa turpis id lacus. Pellentesque non eros quis nisi lobortis pulvinar. Donec et dui at mi eleifend tincidunt. Curabitur ultricies quam non justo varius.'
+        // ];
 
-        $p2 = [
-            'name' => 'PHP DEVELOMENT in depth',
-            'image' => 'uploads/products/book.png',
-            'price' => 2400,
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum et magna vitae iaculis. Aenean efficitur, nisi at rhoncus pulvinar, augue nunc pretium magna, eget mattis massa turpis id lacus. Pellentesque non eros quis nisi lobortis pulvinar. Donec et dui at mi eleifend tincidunt. Curabitur ultricies quam non justo varius.'
-        ];
+        // $p2 = [
+        //     'name' => 'PHP DEVELOMENT in depth',
+        //     'image' => 'uploads/products/book.png',
+        //     'price' => 2400,
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum et magna vitae iaculis. Aenean efficitur, nisi at rhoncus pulvinar, augue nunc pretium magna, eget mattis massa turpis id lacus. Pellentesque non eros quis nisi lobortis pulvinar. Donec et dui at mi eleifend tincidunt. Curabitur ultricies quam non justo varius.'
+        // ];
 
 
-        Product::create($p1);
-        Product::create($p2);
+        // Product::create($p1);
+        // Product::create($p2);
+
+        factory(\App\Product::class, 30)->create();
     }
 }
